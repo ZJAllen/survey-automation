6c023d9fb05b7d6b6649a8e53a17b16bc8174bf2
ZJAllen <zach.allen92@gmail.com>
2021-04-07 12:26:21 -0400

    initial commit. working program

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# survey-automation
+# SurveyMonkey Automation
+
 Automate a simple SurveyMonkey form
+
+# Installation
+
+## 1. Get the Chrome webdrivers
+
+Head to [https://chromedriver.chromium.org/downloads](https://chromedriver.chromium.org/downloads) and follow the link according to your Chrome driver.
+
+For example, if your Chrome version is 89, go [here](https://chromedriver.storage.googleapis.com/index.html?path=89.0.4389.23/) and download the driver for your machine.
+
+Unzip the file, and copy the `chromedriver.exe` file to somewhere on your machine's `PATH`. For example, if you are on Windows, copy it to `C:\Windows`. This way, Selenium will be able to find the correct driver without any further setup.
+
+## 2. Create a virtual enviroment
+
+Steps 2-4 are optional but recommended.
+
+The exact syntax depends on how you start Python on your machine. It could be either of the following:
+
+1. `python -m venv venv`
+2. `py -m venv venv`
+3. `python3 -m venv venv`
+4. `py3 -m venv venv`
+
+## 3. Activate your virtual environment
+
+On Windows: `venv\Scripts\activate`
+On Mac/Linux: `source venv/bin/activate`
+
+(At any time, you can deactivate the virtual environment by typing `deactivate` in the terminal or by closing the terminal instance)
+
+## 4. Upgrade pip...because it'll ask you to
+
+`python -m pip install --upgrade pip`
+
+## 5. Install the packages from the `requirements.txt`
+
+`python -m pip install -r requirements.txt`
+
+---
+
+# Make sure you're all set
+
+This is optional but will be a quick way to make sure your driver is ready to go.
+
+While your virtual environment is activated start a Python shell and run the first import statement.
+`>>> from selenium.webdriver import Chrome`
+
+If you don't get any errors, you're good to go!
+
+---
+
+# Running the program
+
+1. Update the `open_time` and `submit_time` variable according to your use case.
+2. Update the `survey_url` variable to fit your survey. The default value is a simple test survey that I made.
+3. In your terminal, run `python survey_automation.py`
+4. Once the current time matches the `open_time`, a Chrome browser window will open. You must stay in this window since it is the only window the program has ahold of.
+5. Fill out the information.
+6. Once the current time matches the `submit_time` the form will be submitted and the window will be closed.
---
+++ survey_automation.py
@@ -0,0 +1,72 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from datetime import date, time, datetime
+
+# Booleans for the while loops below.
+running = True
+start = True
+submit = False
+
+# This is the time the browser will open and navigate to the form.
+# Update these to fit your needs
+# datetime(YEAR, MONTH, DAY, HOUR, MINUTE, SECOND)
+open_time = datetime(2021, 4, 7, 11, 50, 0)
+
+# This is the time the browser will submit the form.
+# Update these to fit your needs
+# datetime(YEAR, MONTH, DAY, HOUR, MINUTE, SECOND, MICROSECOND)
+submit_time = datetime(2021, 4, 7, 11, 5, 0, 100)
+
+# Enter the survey URL here. This is just a dummy survey.
+survey_url = "https://www.surveymonkey.com/r/PZCCPLC"
+
+# # These are just for testing. Uncomment as needed.
+# name_input = "Last, First"
+# number_input = "123456789"
+
+opts = Options()
+
+while running:
+    while start:
+        if datetime.now() >= open_time:
+            # This triggers at the start time
+            # This way it will open the browser ahead of time so you
+            # can fill in the info.
+
+            # Instantiate the brower instance
+            browser = Chrome(options=opts)
+
+            # Navigate to the survey url
+            browser.get(survey_url)
+
+            # Grabs ahold of the survey form for later.
+            application_form = browser.find_element_by_name("surveyForm")
+
+            # # These are just for testing. I got tired of typing them in manually.
+            # # Uncomment as needed.
+            # first_input = browser.find_elements_by_tag_name("input")[0]
+            # first_input.send_keys(name_input)
+            # second_input = browser.find_elements_by_tag_name("input")[1]
+            # second_input.send_keys(number_input)
+
+            # Set the start bool to True to let the program know we've opened
+            # the form and are waiting for the submit time
+            start = False
+
+    while not submit:
+        if datetime.now() >= submit_time:
+            # This triggers at the submit time
+            # Make sure all the required fields are populated and
+            # the program will do the rest
+
+            # This will submit the form
+            application_form.submit()
+
+            # Change the flags so we can get out of these nested While loops
+            submit = True
+            running = False
+
+
+# This closes the webdriver. Otherwise you end up with a
+# ton of open Chrome instances and it really bogs you down.
+browser.quit()
